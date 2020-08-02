62e3e3d5c94ac0550a07e2d20eb2b138f4310f8f
Russ Perry <rperry1886@gmail.com>
2020-08-02 12:32:16 -0400

    Add deleting bookmarks

--- app.js
+++ app.js
@@ -16,6 +16,45 @@ function showModal() {
   websiteNameEl.focus();
 }
 
+// Build bookmarks DOM
+function buildBookmarks() {
+  // Remove all bookmark elements before appending
+  bookmarksContainer.textContent = '';
+
+  // Build items
+  bookmarks.forEach((bookmark) => {
+    const { name, url } = bookmark;
+    // Item
+    const item = document.createElement('div');
+    item.classList.add('item');
+    // Close Icon
+    const closeIcon = document.createElement('i');
+    closeIcon.classList.add('fa', 'fa-times');
+    closeIcon.setAttribute('title', 'Delete Bookmark');
+    closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
+    // Favicon / Link Container
+    const linkInfo = document.createElement('div');
+    linkInfo.classList.add('name');
+    // Favicon
+    const favicon = document.createElement('img');
+    favicon.setAttribute(
+      'src',
+      `https://s2.googleusercontent.com/s2/favicons?domain=${url}`
+    );
+    favicon.setAttribute('alt', 'Favicon');
+    // Link
+    const link = document.createElement('a');
+    link.setAttribute('href', `${url}`);
+    link.setAttribute('target', '_blank');
+    link.textContent = name;
+
+    // Append to bookmarks container
+    linkInfo.append(favicon, link);
+    item.append(closeIcon, linkInfo);
+    bookmarksContainer.appendChild(item);
+  });
+}
+
 // Fetch bookmarks
 function fetchBookmarks() {
   // Get bookmarks from localStorage if available
@@ -31,6 +70,21 @@ function fetchBookmarks() {
     ];
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
   }
+  buildBookmarks();
+}
+
+// Delete a bookmark
+function deleteBookmark(url) {
+  bookmarks.forEach((bookmark, index) => {
+    if (bookmark.url === url) {
+      bookmarks.splice(index, 1);
+    } else {
+      console.log('None Found');
+    }
+  });
+  // Update bookmarks array in localStorage, re-populate DOM
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  fetchBookmarks();
 }
 
 // Handle Data from form
@@ -50,7 +104,8 @@ function storeBookmark(e) {
   };
   bookmarks.push(bookmark);
   localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-  mookmarkForm.reset();
+  buildBookmarks();
+  bookmarkForm.reset();
   websiteNameEl.focus();
 }
 
